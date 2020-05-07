0910353ac289cf4072f1cec59113f8f23e733f37
noahjutz <noah.jutz@gmail.com>
2020-05-07 22:45:20 +0200

    Add ExerciseWrapperDao

---
+++ app/src/main/java/com/noahjutz/gymroutines/data/dao/ExerciseWrapperDao.kt
@@ -0,0 +1,23 @@
+package com.noahjutz.gymroutines.data.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.noahjutz.gymroutines.data.ExerciseWrapper
+
+@Suppress("unused")
+private const val TAG = "ExerciseWrapperDao"
+
+@Dao
+abstract class ExerciseWrapperDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    abstract suspend fun insert(exerciseWrapper: ExerciseWrapper)
+
+    @Delete
+    abstract suspend fun delete(exerciseWrapper: ExerciseWrapper)
+
+    @Update
+    abstract suspend fun update(exerciseWrapper: ExerciseWrapper)
+
+    @Query("SELECT * FROM exercise_wrapper_table")
+    abstract suspend fun getExerciseWrappers(): LiveData<List<ExerciseWrapper>>
+}
