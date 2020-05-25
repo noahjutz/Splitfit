d69946486485850e28b539dc791e1f7a244e3222
noahjutz <noah.jutz@gmail.com>
2020-05-25 14:33:29 +0200

    Add SetDao

---
+++ app/src/main/java/com/noahjutz/gymroutines/data/dao/SetDao.kt
@@ -0,0 +1,23 @@
+package com.noahjutz.gymroutines.data.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.noahjutz.gymroutines.data.Set
+
+@Suppress("unused")
+private const val TAG = "SetDao"
+
+@Dao
+interface SetDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(set: Set): Long
+
+    @Delete
+    suspend fun delete(set: Set)
+
+    @Query("SELECT * FROM set_table WHERE setId == :id")
+    suspend fun getSetById(id: Int): Set?
+
+    @Query("SELECT * FROM set_table")
+    suspend fun getSets(): LiveData<List<Set>>
+}
