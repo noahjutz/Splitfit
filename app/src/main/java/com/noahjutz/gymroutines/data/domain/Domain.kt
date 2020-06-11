ba9135f61329bbf53ce34cb5375fe67f0f114181
Noah Jutz <noah.jutz@gmail.com>
2020-06-11 21:10:57 +0200

    Move legacy classes to seperate file in domain

--- app/src/main/java/com/noahjutz/gymroutines/data/domain/Domain.kt
+++ app/src/main/java/com/noahjutz/gymroutines/data/domain/Domain.kt
@@ -21,36 +21,6 @@ data class Exercise(
     val exerciseId: Int = 0
 )
 
-// TODO: Remove RoutineExerciseCrossRef
-@Entity(primaryKeys = ["routineId", "exerciseWrapperId"])
-data class RoutineExerciseCrossRef(
-    val routineId: Int,
-    val exerciseWrapperId: Int
-)
-
-/**
- * [Routine] with [ExerciseWrapper]s
- */
-// TODO: Remove RwE and replace it with RwEwS
-data class RwE(
-    @Embedded val routine: Routine,
-    @Relation(
-        parentColumn = "routineId",
-        entityColumn = "exerciseWrapperId",
-        associateBy = Junction(RoutineExerciseCrossRef::class)
-    )
-    val exerciseWrappers: List<ExerciseWrapper>
-)
-
-@Entity(tableName = "exercise_wrapper_table")
-data class ExerciseWrapper(
-    val exerciseId: Int,
-    val routineId: Int,
-
-    @PrimaryKey(autoGenerate = true)
-    val exerciseWrapperId: Int = 0
-)
-
 @Entity(tableName = "set_table")
 data class Set(
     val exerciseWrapperId: Int,
@@ -62,9 +32,6 @@ data class Set(
 /**
  * [ExerciseWrapper] with [Set]s
  */
-// TODO: Add @Embedded val exercise: Exercise instead of exerciseWrapper
-// TODO: Remove the ExerciseWrapper class because EwS replaces it
-// TODO: Create an associative entity between Exercise and Routine (not ExerciseWrapper)
 data class EwS(
     @Embedded val exerciseWrapper: ExerciseWrapper,
     @Relation(
