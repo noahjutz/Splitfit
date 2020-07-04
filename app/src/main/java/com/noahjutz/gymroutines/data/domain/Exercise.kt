e9dbe5f2878bf76b146e56daa165b49a675c0e7e
Noah Jutz <noah.jutz@gmail.com>
2020-07-04 12:06:26 +0200

    Store which values to log
    - logReps
    - logWeight
    - logTime
    - logDistance

--- app/src/main/java/com/noahjutz/gymroutines/data/domain/Exercise.kt
+++ app/src/main/java/com/noahjutz/gymroutines/data/domain/Exercise.kt
@@ -10,6 +10,10 @@ import androidx.room.PrimaryKey
 data class Exercise(
     var name: String,
     var description: String,
+    var logReps: Boolean = false,
+    var logWeight: Boolean = false,
+    var logTime: Boolean = false,
+    var logDistance: Boolean = false,
     var hidden: Boolean = false,
 
     @PrimaryKey(autoGenerate = true)
