81febe9797aa04f2db87364626a353f605ee7e80
Noah Jutz <noah.jutz@gmail.com>
2020-07-05 11:33:52 +0200

    Add SetAdapter

---
+++ app/src/main/java/com/noahjutz/gymroutines/ui/routines/create/SetAdapter.kt
@@ -0,0 +1,44 @@
+package com.noahjutz.gymroutines.ui.routines.create
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.noahjutz.gymroutines.R
+import com.noahjutz.gymroutines.data.domain.Set
+import kotlinx.android.synthetic.main.listitem_exercise.view.*
+
+private val diffUtil = object : DiffUtil.ItemCallback<Set>() {
+    override fun areItemsTheSame(oldItem: Set, newItem: Set): Boolean {
+        return oldItem == newItem
+    }
+
+    override fun areContentsTheSame(oldItem: Set, newItem: Set): Boolean {
+        return oldItem.distance == newItem.distance
+                && oldItem.reps == newItem.reps
+                && oldItem.time == newItem.time
+                && oldItem.weight == newItem.weight
+    }
+
+}
+
+class SetAdapter : ListAdapter<Set, SetAdapter.SetHolder>(diffUtil) {
+    inner class SetHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SetAdapter.SetHolder {
+        // TODO: Replace listitem
+        val itemView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.listitem_exercise, parent, false)
+        return SetHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: SetAdapter.SetHolder, position: Int) {
+        val set = getItem(position)
+
+        holder.itemView.apply {
+            name.text = "Set $position: Reps: ${set.reps}"
+        }
+    }
+}
