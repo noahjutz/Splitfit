c816fb0decedcef4d299267d1f1d4633f3dd2499
Noah Jutz <noah.jutz@gmail.com>
2020-07-10 13:29:02 +0200

    Update DiffUtilCallback

--- app/src/main/java/com/noahjutz/gymroutines/util/DiffUtilCallback.kt
+++ app/src/main/java/com/noahjutz/gymroutines/util/DiffUtilCallback.kt
@@ -6,11 +6,11 @@ import androidx.recyclerview.widget.DiffUtil
  * Implementation of [DiffUtil.ItemCallback] for data classes.
  */
 class DiffUtilCallback<T : Equatable>(
-    private val itemsEqual: (oldItem: T, newItem: T) -> Boolean = { oldItem, newItem -> oldItem == newItem },
-    private val contentsEqual: (oldItem: T, newItem: T) -> Boolean = { oldItem, newItem -> oldItem == newItem }
+    private val itemsEqual: (old: T, new: T) -> Boolean = { old, new -> old == new },
+    private val contentsEqual: (old: T, new: T) -> Boolean = { old, new -> old == new }
 ) : DiffUtil.ItemCallback<T>() {
-    override fun areItemsTheSame(oldItem: T, newItem: T) = itemsEqual(oldItem, newItem)
-    override fun areContentsTheSame(oldItem: T, newItem: T) = contentsEqual(oldItem, newItem)
+    override fun areItemsTheSame(old: T, new: T) = itemsEqual(old, new)
+    override fun areContentsTheSame(old: T, new: T) = contentsEqual(old, new)
 }
 
 interface Equatable {
