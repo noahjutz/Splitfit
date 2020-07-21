8862b7b1404727af5cf382cd53348ff77c408f4a
Noah Jutz <noah.jutz@gmail.com>
2020-07-21 17:17:38 +0200

    Remove deprecated ViewModelFactory

--- app/src/main/java/com/noahjutz/gymroutines/util/InjectorUtils.kt
+++ app/src/main/java/com/noahjutz/gymroutines/util/InjectorUtils.kt
@@ -7,19 +7,6 @@ import com.noahjutz.gymroutines.data.Repository
 // TODO: Replace with hilt
 @Deprecated("Use hilt DI instead.")
 object InjectorUtils {
-    fun provideViewModelFactory(application: Application): ViewModelFactory {
-        val database = AppDatabase.getInstance(application)
-        val repository = Repository.getInstance(
-            database.exerciseDao,
-            database.exerciseHolderDao,
-            database.exerciseImplDao,
-            database.fullRoutineDao,
-            database.routineDao,
-            database.setDao
-        )
-        return ViewModelFactory(repository)
-    }
-
     fun provideCreateViewModelFactory(application: Application, id: Int): CreateViewModelFactory {
         val database = AppDatabase.getInstance(application)
         val repository = Repository.getInstance(
