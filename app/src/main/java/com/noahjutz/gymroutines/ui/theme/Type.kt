f1e4c6d0976f865b93afcf9baa83a077b674683b
Noah Jutz <noah.jutz@gmail.com>
2020-08-03 19:55:36 +0200

    Remove custom theme package

--- app/src/main/java/com/noahjutz/gymroutines/ui/theme/Type.kt
+++
@@ -1,9 +0,0 @@
-package com.noahjutz.gymroutines.ui.theme
-
-import androidx.ui.material.Typography
-import androidx.ui.text.TextStyle
-import androidx.ui.text.font.FontFamily
-import androidx.ui.text.font.FontWeight
-import androidx.ui.unit.sp
-
-val typography = Typography()
