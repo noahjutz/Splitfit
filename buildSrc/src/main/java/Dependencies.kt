7ff93f21e10d3726ebd13b26198e30fe4916357a
Noah Jutz <noahjutz@protonmail.com>
2020-10-03 14:18:59 +0200

    Update to compose 1.0.0-alpha04
    android-gradle 4.2.0-alpha13

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -28,7 +28,7 @@ object Versions {
     const val gson = "2.8.6"
     const val ktlintPlugin = "9.4.0"
     const val ktlint = "0.39.0"
-    const val gradle = "4.2.0-alpha12"
+    const val gradle = "4.2.0-alpha13"
     const val kotlin = "1.4.10"
     const val hilt = "2.28-alpha"
     const val hiltExt = "1.0.0-alpha02"
@@ -37,7 +37,7 @@ object Versions {
     const val material = "1.2.0"
     const val lifecycle = "2.2.0"
     const val navigation = "2.3.0"
-    const val compose = "1.0.0-alpha03"
+    const val compose = "1.0.0-alpha04"
 
     const val junit = "4.12"
     const val assertJ = "3.11.1"
