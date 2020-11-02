2f58a0cb3723279b2639c03c4565031eca7a6a94
Noah Jutz <noahjutz@tutanota.de>
2020-11-02 16:14:11 +0100

    Limit integer regex pattern to 3 digits

--- app/src/main/java/com/noahjutz/gymroutines/util/NumFormatUtil.kt
+++ app/src/main/java/com/noahjutz/gymroutines/util/NumFormatUtil.kt
@@ -20,7 +20,7 @@ package com.noahjutz.gymroutines.util
 
 object RegexPatterns {
     val integer = """
-        ^$|^0$|^[1-9]\d{0,3}$
+        ^$|^0$|^[1-9]\d{0,2}$
     """.trimIndent().toRegex()
     val float = """
         ^(0|[1-9]\d{0,2})?((?<=\d)\.)?((?<=\.)\d{1,3}$)?
