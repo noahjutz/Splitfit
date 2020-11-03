349a3225fc504b34491991afe7664129411ee993
Noah Jutz <noahjutz@tutanota.de>
2020-11-03 10:27:17 +0100

    Don't use deprecated foundation.Icon
    Instead, use androidx.compose.material.Icon

--- app/src/main/java/com/noahjutz/gymroutines/ui/routines/RoutinesFragment.kt
+++ app/src/main/java/com/noahjutz/gymroutines/ui/routines/RoutinesFragment.kt
@@ -23,7 +23,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.View.VISIBLE
 import android.view.ViewGroup
-import androidx.compose.foundation.Icon
+import androidx.compose.material.Icon
 import androidx.compose.foundation.Text
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
