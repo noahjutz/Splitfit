cf63a3ca31339fd8155f10c96e645d5a102c63e5
Noah Jutz <noahjutz@tutanota.de>
2020-11-19 20:45:38 +0100

    Use Compose for CreateExerciseFragment
    Issue #33

--- app/src/main/java/com/noahjutz/gymroutines/ui/exercises/create/CreateExerciseFragment.kt
+++ app/src/main/java/com/noahjutz/gymroutines/ui/exercises/create/CreateExerciseFragment.kt
@@ -23,14 +23,31 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.View.GONE
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
+import androidx.compose.foundation.ScrollableColumn
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawOpacity
+import androidx.compose.ui.focus.ExperimentalFocus
+import androidx.compose.ui.focus.isFocused
+import androidx.compose.ui.focusObserver
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.viewinterop.viewModel
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.noahjutz.gymroutines.R
-import com.noahjutz.gymroutines.databinding.FragmentCreateExerciseBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -39,32 +56,142 @@ class CreateExerciseFragment : Fragment() {
     private val viewModel: CreateExerciseViewModel by viewModels()
     private val args: CreateExerciseFragmentArgs by navArgs()
 
+    @ExperimentalFocus
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ) = DataBindingUtil.inflate<FragmentCreateExerciseBinding>(
-        inflater, R.layout.fragment_create_exercise, container, false
-    ).apply {
-        fragment = this@CreateExerciseFragment
-        lifecycleOwner = viewLifecycleOwner
-        viewmodel = viewModel
-    }.root
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        initActivity()
-        initViewModel()
-    }
-
-    private fun initActivity() {
-        requireActivity().apply {
-            title = if (args.exerciseId == -1) "Create Exercise" else "Edit Exercise"
-            findViewById<BottomNavigationView>(R.id.bottom_nav).visibility = GONE
+    ) = ComposeView(requireActivity()).apply {
+        setContent {
+            MaterialTheme(colors = if (isSystemInDarkTheme()) darkColors() else lightColors()) {
+                val viewModel = viewModel<CreateExerciseViewModel>()
+                val exercise by viewModel.exercise.observeAsState()
+                // TODO: Merge copy-paste-code from CreateRoutineFragment
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            navigationIcon = {
+                                IconButton(
+                                    onClick = ::popBackStack,
+                                    icon = { Icon(Icons.Default.ArrowBack) },
+                                )
+                            },
+                            title = {
+                                Box {
+                                    var nameFieldValue by remember {
+                                        mutableStateOf(
+                                            TextFieldValue(
+                                                viewModel.exercise.value?.name ?: "Unnamed"
+                                            )
+                                        )
+                                    }
+                                    var focusState by remember { mutableStateOf(false) }
+                                    BasicTextField(
+                                        value = nameFieldValue,
+                                        onValueChange = {
+                                            nameFieldValue = it
+                                            viewModel.updateExercise { name = it.text }
+                                        },
+                                        modifier = Modifier
+                                            .focusObserver {
+                                                focusState = it.isFocused
+                                            }
+                                            .fillMaxWidth(),
+                                        textStyle = AmbientTextStyle.current.copy(
+                                            color = if (isSystemInDarkTheme()) MaterialTheme.colors.onSurface else MaterialTheme.colors.onPrimary
+                                        ),
+                                        maxLines = 1,
+                                        cursorColor = if (isSystemInDarkTheme()) MaterialTheme.colors.onSurface else MaterialTheme.colors.onPrimary
+                                    )
+                                    if (nameFieldValue.text.isEmpty() && !focusState) {
+                                        Text("Unnamed", modifier = Modifier.drawOpacity(0.5f))
+                                    }
+                                }
+                            }
+                        )
+                    },
+                    bodyContent = {
+                        // TODO: Reuse checkbox ListItem component
+                        ScrollableColumn {
+                            var repsChecked by remember { mutableStateOf(exercise!!.logReps) }
+                            onCommit(repsChecked) {
+                                viewModel.updateExercise {
+                                    logReps = repsChecked
+                                }
+                            }
+                            ListItem(
+                                text = { Text("Log Reps") },
+                                icon = {
+                                    Checkbox(
+                                        checked = repsChecked,
+                                        onCheckedChange = { repsChecked = it }
+                                    )
+                                },
+                                modifier = Modifier.clickable { repsChecked = !repsChecked }
+                            )
+                            var weightChecked by remember { mutableStateOf(exercise!!.logWeight) }
+                            onCommit(weightChecked) {
+                                viewModel.updateExercise {
+                                    logWeight = weightChecked
+                                }
+                            }
+                            ListItem(
+                                text = { Text("Log Weight") },
+                                icon = {
+                                    Checkbox(
+                                        checked = weightChecked,
+                                        onCheckedChange = { weightChecked = it }
+                                    )
+                                },
+                                modifier = Modifier.clickable { weightChecked = !weightChecked }
+                            )
+                            var timeChecked by remember { mutableStateOf(exercise!!.logTime) }
+                            onCommit(timeChecked) {
+                                viewModel.updateExercise {
+                                    logTime = timeChecked
+                                }
+                            }
+                            ListItem(
+                                text = { Text("Log Time") },
+                                icon = {
+                                    Checkbox(
+                                        checked = timeChecked,
+                                        onCheckedChange = { timeChecked = it }
+                                    )
+                                },
+                                modifier = Modifier.clickable { timeChecked = !timeChecked }
+                            )
+                            var distanceChecked by remember { mutableStateOf(exercise!!.logDistance) }
+                            onCommit(distanceChecked) {
+                                viewModel.updateExercise {
+                                    logDistance = distanceChecked
+                                }
+                            }
+                            ListItem(
+                                text = { Text("Log Distance") },
+                                icon = {
+                                    Checkbox(
+                                        checked = distanceChecked,
+                                        onCheckedChange = { distanceChecked = it }
+                                    )
+                                },
+                                modifier = Modifier.clickable { distanceChecked = !distanceChecked }
+                            )
+                        }
+                    }
+                )
+            }
         }
     }
 
-    private fun initViewModel() {
-        viewModel.exercise.observe(viewLifecycleOwner, Observer {})
+    private fun popBackStack() {
+        findNavController().popBackStack()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        requireActivity().apply {
+            findViewById<BottomNavigationView>(R.id.bottom_nav).visibility = GONE
+        }
     }
 }
--- app/src/main/java/com/noahjutz/gymroutines/ui/exercises/create/CreateExerciseViewModel.kt
+++ app/src/main/java/com/noahjutz/gymroutines/ui/exercises/create/CreateExerciseViewModel.kt
@@ -24,80 +24,32 @@ import androidx.lifecycle.*
 import com.noahjutz.gymroutines.data.Repository
 import com.noahjutz.gymroutines.data.domain.Exercise
 import com.noahjutz.gymroutines.util.ARGS_EXERCISE_ID
+import com.noahjutz.gymroutines.util.ARGS_ROUTINE_ID
 import kotlinx.coroutines.runBlocking
 
 class CreateExerciseViewModel @ViewModelInject constructor(
     private val repository: Repository,
     @Assisted private val args: SavedStateHandle
 ) : ViewModel() {
-    /**
-     * The [Exercise] object that is being created/edited
-     * @see initExercise: adds [name] and [description] as source
-     * @see save
-     */
     private val _exercise = MediatorLiveData<Exercise>()
     val exercise: LiveData<Exercise>
         get() = _exercise
 
-    /**
-     * Data binding fields
-     * [MediatorLiveData] sources for [exercise]
-     * @see initBinding
-     */
-    val name = MutableLiveData<String>()
-    val logWeight = MutableLiveData<Boolean>()
-    val logReps = MutableLiveData<Boolean>()
-    val logTime = MutableLiveData<Boolean>()
-    val logDistance = MutableLiveData<Boolean>()
-
     init {
         initExercise()
-        initBinding()
-    }
-
-    private fun initBinding() {
-        name.value = exercise.value!!.name
-        logWeight.value = exercise.value!!.logWeight
-        logReps.value = exercise.value!!.logReps
-        logTime.value = exercise.value!!.logTime
-        logDistance.value = exercise.value!!.logDistance
     }
 
     private fun initExercise() {
         _exercise.run {
             value = getExerciseById(args[ARGS_EXERCISE_ID] ?: -1)
                 ?: repository.getExercise(repository.insert(Exercise()).toInt())
-
-            addSource(name) { source ->
-                _exercise.value = _exercise.value!!.apply { name = source.trim() }
-            }
-
-            addSource(logWeight) { source ->
-                _exercise.value = _exercise.value!!.apply { logWeight = source }
-            }
-
-            addSource(logReps) { source ->
-                _exercise.value = _exercise.value!!.apply { logReps = source }
-            }
-
-            addSource(logTime) { source ->
-                _exercise.value = _exercise.value!!.apply { logTime = source }
-            }
-
-            addSource(logDistance) { source ->
-                _exercise.value = _exercise.value!!.apply { logDistance = source }
-            }
         }
-    }
-
-    override fun onCleared() {
-        save()
-    }
-
-    private fun save() {
-        repository.insert(exercise.value!!)
     }
 
     /** [repository] access functions */
     private fun getExerciseById(id: Int): Exercise? = runBlocking { repository.getExercise(id) }
+
+    fun updateExercise(action: Exercise.() -> Unit) {
+        repository.insert(repository.getExercise(args[ARGS_EXERCISE_ID]!!)!!.apply(action))
+    }
 }
