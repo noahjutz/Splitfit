650782951f927dc26c90b34c80d16967f9fdc33c
Noah Jutz <noahjutz@tutanota.de>
2020-11-29 11:30:09 +0100

    Remove PickExerciseFragment class

--- app/src/main/java/com/noahjutz/gymroutines/ui/routines/create/pick/PickExerciseFragment.kt
+++ app/src/main/java/com/noahjutz/gymroutines/ui/routines/create/pick/PickExerciseFragment.kt
@@ -18,11 +18,7 @@
 
 package com.noahjutz.gymroutines.ui.routines.create.pick
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.ViewGroup
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.lazy.LazyColumnFor
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -34,43 +30,7 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.ComposeView
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
-import androidx.navigation.fragment.findNavController
 import com.noahjutz.gymroutines.ui.exercises.ExercisesViewModel
-import dagger.hilt.android.AndroidEntryPoint
-
-@Suppress("unused")
-private const val TAG = "PickExerciseFragment"
-
-@AndroidEntryPoint
-class PickExerciseFragment : Fragment() {
-
-    private val exercisesViewModel: ExercisesViewModel by viewModels()
-    private val sharedExerciseViewModel: SharedExerciseViewModel by activityViewModels()
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ) = ComposeView(requireContext()).apply {
-        setContent {
-            MaterialTheme(colors = if (isSystemInDarkTheme()) darkColors() else lightColors()) {
-                PickExercise(
-                    exercisesViewModel = exercisesViewModel,
-                    sharedExerciseViewModel = sharedExerciseViewModel,
-                    popBackStack = ::popBackStack
-                )
-            }
-        }
-    }
-
-    private fun popBackStack() {
-        findNavController().popBackStack()
-    }
-}
 
 @Composable
 fun PickExercise(
