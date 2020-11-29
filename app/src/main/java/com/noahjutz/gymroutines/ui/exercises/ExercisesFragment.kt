692095581eef3036103a9105156e5bdcb38882a2
Noah Jutz <noahjutz@tutanota.de>
2020-11-29 11:26:05 +0100

    Remove ExercisesFragment class

--- app/src/main/java/com/noahjutz/gymroutines/ui/exercises/ExercisesFragment.kt
+++ app/src/main/java/com/noahjutz/gymroutines/ui/exercises/ExercisesFragment.kt
@@ -18,13 +18,7 @@
 
 package com.noahjutz.gymroutines.ui.exercises
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.View.VISIBLE
-import android.view.ViewGroup
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.lazy.LazyColumnFor
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -32,51 +26,6 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.ComposeView
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
-import androidx.navigation.fragment.findNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.noahjutz.gymroutines.R
-import dagger.hilt.android.AndroidEntryPoint
-
-@AndroidEntryPoint
-class ExercisesFragment : Fragment() {
-
-    private val viewModel: ExercisesViewModel by viewModels()
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ) = ComposeView(requireContext()).apply {
-        // TODO: Implement SwipeToDismiss with AnimatedVisibility
-        setContent {
-            MaterialTheme(colors = if (isSystemInDarkTheme()) darkColors() else lightColors()) {
-                ExercisesScreen(addEditExercise = ::addEditExercise, viewModel = viewModel)
-            }
-        }
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        initActivity()
-    }
-
-    private fun initActivity() {
-        requireActivity().apply {
-            findViewById<BottomNavigationView>(R.id.bottom_nav).visibility = VISIBLE
-        }
-    }
-
-    fun addEditExercise(exerciseId: Int) {
-        val exerciseIdToEdit =
-            if (exerciseId < 1) viewModel.addExercise().toInt()
-            else exerciseId
-        val action = ExercisesFragmentDirections.addExercise(exerciseIdToEdit)
-        findNavController().navigate(action)
-    }
-}
 
 @Composable
 fun ExercisesScreen(
