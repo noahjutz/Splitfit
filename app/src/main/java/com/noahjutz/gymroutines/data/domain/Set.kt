827740366ba1b24d85c198c36a7c07505fd7c87d
Noah Jutz <noahjutz@tutanota.de>
2020-12-05 19:43:59 +0100

    Remove redundant @Entity annotation from Set

--- app/src/main/java/com/noahjutz/gymroutines/data/domain/Set.kt
+++ app/src/main/java/com/noahjutz/gymroutines/data/domain/Set.kt
@@ -18,10 +18,6 @@
 
 package com.noahjutz.gymroutines.data.domain
 
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
-@Entity(tableName = "set_table")
 data class Set(
     val exerciseId: Int,
     var reps: Int? = null,
