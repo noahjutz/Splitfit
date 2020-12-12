33cbadff06f63d7e5ece18b93e25b5ef49773991
Noah Jutz <noahjutz@tutanota.de>
2020-12-12 17:03:22 +0100

    Check logReps by default
    closes #62

--- app/src/main/java/com/noahjutz/gymroutines/data/domain/Exercise.kt
+++ app/src/main/java/com/noahjutz/gymroutines/data/domain/Exercise.kt
@@ -24,7 +24,7 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "exercise_table")
 data class Exercise(
     var name: String = "",
-    var logReps: Boolean = false,
+    var logReps: Boolean = true,
     var logWeight: Boolean = false,
     var logTime: Boolean = false,
     var logDistance: Boolean = false,
