8b0a59d53ff8b015461519e3027e79a57f985069
Noah Jutz <noahjutz@tutanota.de>
2020-12-17 21:23:12 +0100

    Use SwipeToDeleteBackground for RoutinesScreen
    Issue #16

--- app/src/main/java/com/noahjutz/splitfit/ui/routines/RoutinesScreen.kt
+++ app/src/main/java/com/noahjutz/splitfit/ui/routines/RoutinesScreen.kt
@@ -20,24 +20,23 @@ package com.noahjutz.splitfit.ui.routines
 
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.animate
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Delete
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.noahjutz.splitfit.R
+import com.noahjutz.splitfit.util.SwipeToDeleteBackground
 
 @ExperimentalAnimationApi
 @ExperimentalMaterialApi
@@ -61,41 +60,19 @@ fun RoutinesScreen(
 
                 SwipeToDismiss(
                     state = dismissState,
-                    background = {
-                        val direction = dismissState.dismissDirection ?: return@SwipeToDismiss
-                        val alignment = when (direction) {
-                            DismissDirection.StartToEnd -> Alignment.CenterStart
-                            DismissDirection.EndToStart -> Alignment.CenterEnd
-                        }
-                        Box(
-                            modifier = Modifier.fillMaxSize()
-                                .background(Color.Red)
-                                .padding(horizontal = 20.dp),
-                            contentAlignment = alignment
-                        ) {
-                            Icon(Icons.Default.Delete)
-                        }
-                    },
-                    dismissContent = {
-                        Card(
-                            elevation = animate(if (dismissState.dismissDirection != null) 4.dp else 0.dp)
-                        ) {
-                            ListItem(
-                                text = {
-                                    Text(
-                                        text = routine.name.takeIf { it.isNotBlank() }
-                                            ?: stringResource(R.string.unnamed_routine),
-                                        maxLines = 1,
-                                        overflow = TextOverflow.Ellipsis,
-                                    )
-                                },
-                                modifier = Modifier.clickable {
-                                    addEditRoutine(routine.routineId)
-                                }
+                    background = { SwipeToDeleteBackground(dismissState) }
+                ) {
+                    Card(elevation = animate(if (dismissState.dismissDirection != null) 4.dp else 0.dp)) {
+                        ListItem(Modifier.clickable { addEditRoutine(routine.routineId) }) {
+                            Text(
+                                text = routine.name.takeIf { it.isNotBlank() }
+                                    ?: stringResource(R.string.unnamed_routine),
+                                maxLines = 1,
+                                overflow = TextOverflow.Ellipsis,
                             )
                         }
                     }
-                )
+                }
 
                 if (dismissState.value != DismissValue.Default) {
                     AlertDialog(
