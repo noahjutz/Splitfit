8ed366792ece11777b2a640958be49485ae5523b
Noah Jutz <noahjutz@tutanota.de>
2020-12-17 18:39:46 +0100

    Fix FAB overlap for RoutinesScreen
    For #14

--- app/src/main/java/com/noahjutz/splitfit/ui/routines/RoutinesScreen.kt
+++ app/src/main/java/com/noahjutz/splitfit/ui/routines/RoutinesScreen.kt
@@ -24,8 +24,9 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumnFor
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -57,79 +58,85 @@ fun RoutinesScreen(
         }
     ) {
         val routines by viewModel.routines.observeAsState()
-        LazyColumnFor(items = routines ?: emptyList()) { routine ->
-            val dismissState = rememberDismissState()
+        LazyColumn {
+            items(items = routines ?: emptyList()) { routine ->
+                val dismissState = rememberDismissState()
 
-            SwipeToDismiss(
-                state = dismissState,
-                background = {
-                    val direction = dismissState.dismissDirection ?: return@SwipeToDismiss
-                    val alignment = when (direction) {
-                        DismissDirection.StartToEnd -> Alignment.CenterStart
-                        DismissDirection.EndToStart -> Alignment.CenterEnd
-                    }
-                    Box(
-                        modifier = Modifier.fillMaxSize()
-                            .background(Color.Red)
-                            .padding(horizontal = 20.dp),
-                        contentAlignment = alignment
-                    ) {
-                        Icon(Icons.Default.Delete)
-                    }
-                },
-                dismissContent = {
-                    Card(
-                        elevation = animate(if (dismissState.dismissDirection != null) 4.dp else 0.dp)
-                    ) {
-                        ListItem(
-                            text = {
-                                Text(
-                                    text = routine.name.takeIf { it.isNotBlank() }
-                                        ?: stringResource(R.string.unnamed_routine),
-                                    maxLines = 1,
-                                    overflow = TextOverflow.Ellipsis,
-                                )
-                            },
-                            modifier = Modifier.clickable {
-                                addEditRoutine(routine.routineId)
-                            }
-                        )
-                    }
-                }
-            )
-
-            if (dismissState.value != DismissValue.Default) {
-                AlertDialog(
-                    title = {
-                        Text(
-                            stringResource(
-                                R.string.confirm_delete,
-                                routine.name.takeIf { it.isNotBlank() }
-                                    ?: stringResource(R.string.unnamed_routine)
+                SwipeToDismiss(
+                    state = dismissState,
+                    background = {
+                        val direction = dismissState.dismissDirection ?: return@SwipeToDismiss
+                        val alignment = when (direction) {
+                            DismissDirection.StartToEnd -> Alignment.CenterStart
+                            DismissDirection.EndToStart -> Alignment.CenterEnd
+                        }
+                        Box(
+                            modifier = Modifier.fillMaxSize()
+                                .background(Color.Red)
+                                .padding(horizontal = 20.dp),
+                            contentAlignment = alignment
+                        ) {
+                            Icon(Icons.Default.Delete)
+                        }
+                    },
+                    dismissContent = {
+                        Card(
+                            elevation = animate(if (dismissState.dismissDirection != null) 4.dp else 0.dp)
+                        ) {
+                            ListItem(
+                                text = {
+                                    Text(
+                                        text = routine.name.takeIf { it.isNotBlank() }
+                                            ?: stringResource(R.string.unnamed_routine),
+                                        maxLines = 1,
+                                        overflow = TextOverflow.Ellipsis,
+                                    )
+                                },
+                                modifier = Modifier.clickable {
+                                    addEditRoutine(routine.routineId)
+                                }
                             )
-                        )
-                    },
-                    confirmButton = {
-                        Button(
-                            onClick = {
-                                viewModel.deleteRoutine(routine.routineId)
-                                dismissState.snapTo(DismissValue.Default)
-                            },
-                            content = { Text(stringResource(R.string.yes)) }
-                        )
-                    },
-                    dismissButton = {
-                        TextButton(
-                            onClick = {
-                                dismissState.snapTo(DismissValue.Default)
-                            },
-                            content = { Text(stringResource(R.string.cancel)) }
-                        )
-                    },
-                    onDismissRequest = {
-                        dismissState.snapTo(DismissValue.Default)
+                        }
                     }
                 )
+
+                if (dismissState.value != DismissValue.Default) {
+                    AlertDialog(
+                        title = {
+                            Text(
+                                stringResource(
+                                    R.string.confirm_delete,
+                                    routine.name.takeIf { it.isNotBlank() }
+                                        ?: stringResource(R.string.unnamed_routine)
+                                )
+                            )
+                        },
+                        confirmButton = {
+                            Button(
+                                onClick = {
+                                    viewModel.deleteRoutine(routine.routineId)
+                                    dismissState.snapTo(DismissValue.Default)
+                                },
+                                content = { Text(stringResource(R.string.yes)) }
+                            )
+                        },
+                        dismissButton = {
+                            TextButton(
+                                onClick = {
+                                    dismissState.snapTo(DismissValue.Default)
+                                },
+                                content = { Text(stringResource(R.string.cancel)) }
+                            )
+                        },
+                        onDismissRequest = {
+                            dismissState.snapTo(DismissValue.Default)
+                        }
+                    )
+                }
+            }
+            item {
+                // Fix FAB overlap
+                Box(Modifier.height(72.dp)) {}
             }
         }
     }
