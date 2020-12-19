5475bebd342899feafd701a90d4852205fd8d6ea
Noah Jutz <noahjutz@tutanota.de>
2020-12-19 20:07:31 +0100

    Allow for cancelling in PickExercise (closes #7)

--- app/src/main/java/com/noahjutz/splitfit/ui/routines/create/pick/PickExerciseScreen.kt
+++ app/src/main/java/com/noahjutz/splitfit/ui/routines/create/pick/PickExerciseScreen.kt
@@ -18,6 +18,7 @@
 
 package com.noahjutz.splitfit.ui.routines.create.pick
 
+import androidx.compose.animation.*
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -25,26 +26,34 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Done
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.noahjutz.splitfit.ui.exercises.ExercisesViewModel
 
+@ExperimentalAnimationApi
 @Composable
 fun PickExerciseScreen(
     exercisesViewModel: ExercisesViewModel,
     sharedExerciseViewModel: SharedExerciseViewModel,
     popBackStack: () -> Unit,
 ) {
+    var save = false
+    onDispose {
+        if (!save) sharedExerciseViewModel.clear()
+    }
+
     Scaffold(
         topBar = {
             TopAppBar(
                 navigationIcon = {
                     IconButton(
                         onClick = popBackStack,
-                        content = { Icon(Icons.Default.ArrowBack) }
+                        content = { Icon(Icons.Default.Close) }
                     )
                 },
                 title = {
@@ -52,6 +61,18 @@ fun PickExerciseScreen(
                 }
             )
         },
+        floatingActionButton = {
+            val selectedExercises by sharedExerciseViewModel.exercises.collectAsState()
+            AnimatedVisibility(
+                visible = selectedExercises.isNotEmpty(),
+                enter = slideInHorizontally({ it * 2 }),
+                exit = fadeOut()
+            ) {
+                FloatingActionButton(onClick = { save = true; popBackStack() }) {
+                    Icon(Icons.Default.Done)
+                }
+            }
+        }
     ) {
         val exercises by exercisesViewModel.exercises.observeAsState()
         LazyColumn(Modifier.fillMaxHeight()) {
