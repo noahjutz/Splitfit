b58ab37162837e02378255d6bcc822f7e49a5b12
Noah Jutz <noahjutz@tutanota.de>
2020-12-23 12:14:40 +0100

    Add NumFormatUtil.kt

---
+++ app/src/main/java/com/noahjutz/splitfit/util/NumFormatUtil.kt
@@ -0,0 +1,32 @@
+/*
+ * Splitfit
+ * Copyright (C) 2020  Noah Jutz
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.noahjutz.splitfit.util
+
+import kotlin.math.floor
+
+object NumFormatUtil {
+    fun Double?.formatString(): String {
+        // Handle null
+        if (this == null) return ""
+        // Handle
+        if (this == floor(this)) return this.toInt().toString()
+        // Anything else
+        return this.toString()
+    }
+}
