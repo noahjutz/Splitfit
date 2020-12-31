4ee381c0df99e9feef1d731d9f5548b8e3591b93
Noah Jutz <noahjutz@tutanota.de>
2020-12-31 19:37:13 +0100

    Add WorkoutDao

---
+++ app/src/main/java/com/noahjutz/splitfit/data/dao/WorkoutDao.kt
@@ -0,0 +1,35 @@
+/*
+ * Splitfit
+ * Copyright (C) 2020  Noah Jutz
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.noahjutz.splitfit.data.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.noahjutz.splitfit.data.domain.Workout
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface WorkoutDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(workout: Workout): Long
+
+    @Query("SELECT * FROM workout_table")
+    fun getWorkouts(): Flow<List<Workout>>
+}
