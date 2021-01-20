945c2dbfb8735ecceaec0b745b1ef5e0734db95c
Noah Jutz <noahjutz@tutanota.de>
2021-01-20 21:27:20 +0100

    Update version to 1.0.0-dev5

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -21,8 +21,8 @@ object App {
     const val compileSdk = 30
     const val minSdk = 21
     const val targetSdk = 30
-    const val versionCode = 9
-    const val versionName = "0.1.0-dev4"
+    const val versionCode = 10
+    const val versionName = "0.1.0-dev5"
 }
 
 object Versions {
