34c2d51d376aa91e4948e73c6c34b02ffc30854c
Noah Jutz <noahjutz@tutanota.de>
2021-01-29 14:12:07 +0100

    Upgrade compose navigation to v1.0.0-alpha06

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -66,7 +66,7 @@ object Libs {
 
     object Navigation {
         private const val version = "2.3.1"
-        private const val versionCompose = "1.0.0-alpha04"
+        private const val versionCompose = "1.0.0-alpha06"
         const val navigation = "androidx.navigation:navigation-fragment-ktx:$version"
         const val compose = "androidx.navigation:navigation-compose:$versionCompose"
     }
