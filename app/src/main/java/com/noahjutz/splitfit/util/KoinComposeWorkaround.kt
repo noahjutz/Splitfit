c5a9896e34754ca04b6d4d9027e861e40dbb316f
Noah Jutz <noahjutz@tutanota.de>
2021-02-14 16:03:09 +0100

    Add KoinComposeWorkaround for compose alpha12

---
+++ app/src/main/java/com/noahjutz/splitfit/util/KoinComposeWorkaround.kt
@@ -0,0 +1,79 @@
+/*
+ * Splitfit
+ * Copyright (C) 2020  Noah Jutz
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.noahjutz.splitfit.util
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.AmbientViewModelStoreOwner
+import androidx.lifecycle.ViewModel
+import org.koin.androidx.viewmodel.ViewModelOwner.Companion.from
+import org.koin.androidx.viewmodel.koin.getViewModel
+import org.koin.core.Koin
+import org.koin.core.context.GlobalContext
+import org.koin.core.parameter.ParametersDefinition
+import org.koin.core.qualifier.Qualifier
+
+/**
+ * Compose v1.0.0-alpha12 and koin v2.2.2 are not compatible. This file contains koin source code as
+ * a temporary workaround.
+ */
+
+/**
+ * Resolve ViewModel instance
+ *
+ * @param qualifier
+ * @param parameters
+ *
+ * @author Arnaud Giuliani
+ */
+@Composable
+inline fun <reified T : ViewModel> getViewModel(
+    qualifier: Qualifier? = null,
+    noinline parameters: ParametersDefinition? = null,
+): T {
+    val owner = AmbientViewModelStoreOwner.current.viewModelStore
+    return remember {
+        GlobalContext.get()
+            .getViewModel(qualifier, owner = { from(owner) }, parameters = parameters)
+    }
+}
+
+/**
+ * Resolve a dependency for [Composable] functions
+ * @param qualifier
+ * @param parameters
+ *
+ * @return Lazy instance of type T
+ *
+ * @author Arnaud Giuliani
+ * @author Henrique Horbovyi
+ * @author Arnaud Giuliani
+ */
+@Composable
+inline fun <reified T> get(
+    qualifier: Qualifier? = null,
+    noinline parameters: ParametersDefinition? = null,
+): T = remember {
+    GlobalContext.get().get(qualifier, parameters)
+}
+
+@Composable
+fun getKoin(): Koin = remember {
+    GlobalContext.get()
+}
