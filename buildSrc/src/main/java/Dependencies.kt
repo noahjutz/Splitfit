8bef17f4e35b4c300768572446d3a90817a94c7c
Noah Jutz <noahjutz@tutanota.de>
2021-03-12 20:19:20 +0100

    Upgrade AGP

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -122,7 +122,7 @@ object TestLibs {
 
 object GradlePlugins {
     object Android {
-        private const val version = "7.0.0-alpha08"
+        private const val version = "7.0.0-alpha09"
         const val classpath = "com.android.tools.build:gradle:$version"
     }
 
