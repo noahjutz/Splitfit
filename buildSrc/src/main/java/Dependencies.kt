acbcf6fb89d8a8ef17bd7f14441b8406ea5838b1
Noah Jutz <noahjutz@tutanota.de>
2021-04-09 21:12:47 +0200

    Upgrade AGP to v7.0.0-alpha14
    And Gradle to v7.0-rc-1

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -121,7 +121,7 @@ object TestLibs {
 
 object GradlePlugins {
     object Android {
-        private const val version = "7.0.0-alpha13"
+        private const val version = "7.0.0-alpha14"
         const val classpath = "com.android.tools.build:gradle:$version"
     }
 
