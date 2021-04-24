a0a8b1904cb86db866011a3ac45e8846c435ef63
Noah Jutz <noahjutz@tutanota.de>
2021-04-24 13:36:40 +0200

    Upgrade Material to v1.3.0

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -39,7 +39,7 @@ object Libs {
     }
 
     object Material {
-        private const val version = "1.2.1"
+        private const val version = "1.3.0"
         const val material = "com.google.android.material:material:$version"
     }
 
