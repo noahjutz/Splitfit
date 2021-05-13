e9783bb0b8ec678b15c01d0d6db5be5097560e2d
Noah Jutz <noahjutz@tutanota.de>
2021-05-13 16:26:15 +0200

    Add custom color palette, add black theme

---
+++ app/src/main/java/com/noahjutz/splitfit/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.noahjutz.splitfit.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Primary = Color(0xFF607d8b)
+val PrimaryDark = Color(0xFF34515e)
+val PrimaryDesaturated = Color(0xFFb0bec5)
+val Secondary = Color(0xFFFF5252)
+val SecondaryDark = Color(0xFFC50E29)
--- app/src/main/java/com/noahjutz/splitfit/ui/theme/Theme.kt
+++ app/src/main/java/com/noahjutz/splitfit/ui/theme/Theme.kt
@@ -5,13 +5,31 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import com.noahjutz.splitfit.ui.settings.Theme
 
-private val DarkColorPalette = darkColors()
+private val DarkColorPalette = darkColors(
+    primary = PrimaryDesaturated,
+    primaryVariant = PrimaryDark,
+    secondary = Secondary,
+    secondaryVariant = Secondary,
+    onPrimary = Color.Black,
+    onSecondary = Color.Black,
+)
 
-private val LightColorPalette = lightColors()
+private val LightColorPalette = lightColors(
+    primary = Primary,
+    primaryVariant = PrimaryDark,
+    secondary = Secondary,
+    secondaryVariant = SecondaryDark,
+    onPrimary = Color.Black,
+    onSecondary = Color.Black,
+)
 
-private val BlackColorPalette = DarkColorPalette
+private val BlackColorPalette = DarkColorPalette.copy(
+    surface = Color.Black,
+    background = Color.Black,
+)
 
 @Composable
 fun SplitfitTheme(
