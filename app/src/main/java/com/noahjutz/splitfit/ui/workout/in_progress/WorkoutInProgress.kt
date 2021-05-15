3a5257a9fb02f8aab2ace69203ad1f438439ff41
Noah Jutz <noahjutz@tutanota.de>
2021-05-15 21:30:00 +0200

    Use ExercisePickerSheet in WorkoutInProgress

--- app/src/main/java/com/noahjutz/splitfit/ui/workout/in_progress/WorkoutInProgress.kt
+++ app/src/main/java/com/noahjutz/splitfit/ui/workout/in_progress/WorkoutInProgress.kt
@@ -29,10 +29,12 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.noahjutz.splitfit.R
 import com.noahjutz.splitfit.ui.components.AppBarTextField
+import com.noahjutz.splitfit.ui.exercises.picker.ExercisePickerSheet
 import com.noahjutz.splitfit.ui.exercises.picker.SharedExercisePickerViewModel
 import kotlinx.coroutines.launch
 import org.koin.androidx.compose.getViewModel
@@ -51,166 +53,181 @@ fun WorkoutInProgress(
     sharedExercisePickerViewModel: SharedExercisePickerViewModel,
 ) {
     val scope = rememberCoroutineScope()
+    val sheetState = rememberModalBottomSheetState(initialValue = ModalBottomSheetValue.Hidden)
 
-    DisposableEffect(null) {
-        scope.launch {
-            viewModel.editor.addExercises(sharedExercisePickerViewModel.exercises.value)
-            sharedExercisePickerViewModel.clear()
-        }
-        onDispose {}
-    }
-
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                navigationIcon = {
-                    IconButton(onClick = popBackStack) { Icon(Icons.Default.Close, null) }
-                },
-                title = {
-                    var nameFieldValue by remember { mutableStateOf(viewModel.presenter.workout.value.name) }
-                    AppBarTextField(
-                        value = nameFieldValue,
-                        onValueChange = {
-                            nameFieldValue = it
-                            viewModel.editor.setName(it)
-                        },
-                        hint = stringResource(R.string.unnamed_workout),
-                    )
-                }
-            )
-        },
-    ) {
-        var showFinishWorkoutDialog by remember { mutableStateOf(false) }
-        if (showFinishWorkoutDialog) FinishWorkoutDialog(
-            onDismiss = {
-                showFinishWorkoutDialog = false
-            },
-            finishWorkout = {
-                viewModel.editor.finishWorkout()
-                popBackStack()
-            }
-        )
-        var showCancelWorkoutDialog by remember { mutableStateOf(false) }
-        if (showCancelWorkoutDialog) CancelWorkoutDialog(
-            onDismiss = {
-                showCancelWorkoutDialog = false
-            },
-            cancelWorkout = {
+    ModalBottomSheetLayout(
+        sheetState = sheetState,
+        scrimColor = Color.Black.copy(alpha = 0.32f),
+        sheetContent = {
+            ExercisePickerSheet(onExercisesSelected = {
                 scope.launch {
-                    viewModel.editor.cancelWorkout()
-                    popBackStack()
+                    viewModel.editor.addExercises(it)
+                    sheetState.hide()
                 }
-            }
-        )
-
-        val workout by viewModel.presenter.workout.collectAsState()
-        val setGroups = workout.setGroups
-        LazyColumn(Modifier.fillMaxHeight()) {
-            item {
-                Column {
-                    val duration by viewModel.presenter.durationString.collectAsState("00:00:00")
-                    ListItem(
-                        modifier = Modifier.clickable {},
-                        text = { Text(duration) },
-                        icon = { Icon(Icons.Default.AccessTime, null) },
-                    )
-                }
-                Divider()
-            }
-
-            itemsIndexed(setGroups) { setGroupIndex, setGroup ->
-                val exercise = viewModel.presenter.getExercise(setGroup.exerciseId)!!
-                com.noahjutz.splitfit.ui.components.SetGroupCard(
-                    name = exercise.name.takeIf { it.isNotBlank() }
-                        ?: stringResource(R.string.unnamed_exercise),
-                    sets = setGroup.sets,
-                    onMoveDown = {
-                        viewModel.editor.swapSetGroups(
-                            setGroupIndex,
-                            setGroupIndex + 1
-                        )
+            })
+        }
+    ) {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    navigationIcon = {
+                        IconButton(onClick = popBackStack) { Icon(Icons.Default.Close, null) }
                     },
-                    onMoveUp = { viewModel.editor.swapSetGroups(setGroupIndex, setGroupIndex - 1) },
-                    onAddSet = { viewModel.editor.addSetTo(setGroup) },
-                    onDeleteSet = { viewModel.editor.deleteSetFrom(setGroup, it) },
-                    logReps = exercise.logReps,
-                    logWeight = exercise.logWeight,
-                    logTime = exercise.logTime,
-                    logDistance = exercise.logDistance,
-                    showCheckbox = true,
-                    onWeightChange = { setIndex, weight ->
-                        viewModel.editor.updateSet(
-                            setGroupIndex,
-                            setIndex,
-                            weight = weight.toDoubleOrNull()
+                    title = {
+                        var nameFieldValue by remember { mutableStateOf(viewModel.presenter.workout.value.name) }
+                        AppBarTextField(
+                            value = nameFieldValue,
+                            onValueChange = {
+                                nameFieldValue = it
+                                viewModel.editor.setName(it)
+                            },
+                            hint = stringResource(R.string.unnamed_workout),
                         )
-                    },
-                    onTimeChange = { setIndex, time ->
-                        viewModel.editor.updateSet(
-                            setGroupIndex,
-                            setIndex,
-                            time = time.toIntOrNull()
-                        )
-                    },
-                    onRepsChange = { setIndex, reps ->
-                        viewModel.editor.updateSet(
-                            setGroupIndex,
-                            setIndex,
-                            reps = reps.toIntOrNull()
-                        )
-                    },
-                    onDistanceChange = { setIndex, distance ->
-                        viewModel.editor.updateSet(
-                            setGroupIndex,
-                            setIndex,
-                            distance = distance.toDoubleOrNull()
-                        )
-                    },
-                    onCheckboxChange = { setIndex, checked ->
-                        viewModel.editor.updateSet(setGroupIndex, setIndex, complete = checked)
                     }
                 )
-            }
-
-            item {
-                Spacer(Modifier.height(16.dp))
-                OutlinedButton(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(horizontal = 8.dp)
-                        .height(120.dp),
-                    onClick = navToPickExercise
-                ) {
-                    Icon(Icons.Default.Add, null)
-                    Spacer(Modifier.width(12.dp))
-                    Text("Add Exercise")
+            },
+        ) {
+            var showFinishWorkoutDialog by remember { mutableStateOf(false) }
+            if (showFinishWorkoutDialog) FinishWorkoutDialog(
+                onDismiss = {
+                    showFinishWorkoutDialog = false
+                },
+                finishWorkout = {
+                    viewModel.editor.finishWorkout()
+                    popBackStack()
                 }
-            }
-
-            item {
-                Spacer(Modifier.height(16.dp))
-                Row(
-                    Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 16.dp),
-                    horizontalArrangement = Arrangement.Center,
-                ) {
-                    TextButton(
-                        modifier = Modifier.weight(1f),
-                        onClick = { showCancelWorkoutDialog = true },
-                    ) {
-                        Icon(Icons.Default.Delete, null)
-                        Spacer(Modifier.width(8.dp))
-                        Text("Delete Workout")
+            )
+            var showCancelWorkoutDialog by remember { mutableStateOf(false) }
+            if (showCancelWorkoutDialog) CancelWorkoutDialog(
+                onDismiss = {
+                    showCancelWorkoutDialog = false
+                },
+                cancelWorkout = {
+                    scope.launch {
+                        viewModel.editor.cancelWorkout()
+                        popBackStack()
                     }
-                    Spacer(Modifier.width(16.dp))
-                    Button(
-                        modifier = Modifier.weight(1f),
-                        onClick = { showFinishWorkoutDialog = true }
+                }
+            )
+
+            val workout by viewModel.presenter.workout.collectAsState()
+            val setGroups = workout.setGroups
+            LazyColumn(Modifier.fillMaxHeight()) {
+                item {
+                    Column {
+                        val duration by viewModel.presenter.durationString.collectAsState("00:00:00")
+                        ListItem(
+                            modifier = Modifier.clickable {},
+                            text = { Text(duration) },
+                            icon = { Icon(Icons.Default.AccessTime, null) },
+                        )
+                    }
+                    Divider()
+                }
+
+                itemsIndexed(setGroups) { setGroupIndex, setGroup ->
+                    val exercise = viewModel.presenter.getExercise(setGroup.exerciseId)!!
+                    com.noahjutz.splitfit.ui.components.SetGroupCard(
+                        name = exercise.name.takeIf { it.isNotBlank() }
+                            ?: stringResource(R.string.unnamed_exercise),
+                        sets = setGroup.sets,
+                        onMoveDown = {
+                            viewModel.editor.swapSetGroups(
+                                setGroupIndex,
+                                setGroupIndex + 1
+                            )
+                        },
+                        onMoveUp = {
+                            viewModel.editor.swapSetGroups(
+                                setGroupIndex,
+                                setGroupIndex - 1
+                            )
+                        },
+                        onAddSet = { viewModel.editor.addSetTo(setGroup) },
+                        onDeleteSet = { viewModel.editor.deleteSetFrom(setGroup, it) },
+                        logReps = exercise.logReps,
+                        logWeight = exercise.logWeight,
+                        logTime = exercise.logTime,
+                        logDistance = exercise.logDistance,
+                        showCheckbox = true,
+                        onWeightChange = { setIndex, weight ->
+                            viewModel.editor.updateSet(
+                                setGroupIndex,
+                                setIndex,
+                                weight = weight.toDoubleOrNull()
+                            )
+                        },
+                        onTimeChange = { setIndex, time ->
+                            viewModel.editor.updateSet(
+                                setGroupIndex,
+                                setIndex,
+                                time = time.toIntOrNull()
+                            )
+                        },
+                        onRepsChange = { setIndex, reps ->
+                            viewModel.editor.updateSet(
+                                setGroupIndex,
+                                setIndex,
+                                reps = reps.toIntOrNull()
+                            )
+                        },
+                        onDistanceChange = { setIndex, distance ->
+                            viewModel.editor.updateSet(
+                                setGroupIndex,
+                                setIndex,
+                                distance = distance.toDoubleOrNull()
+                            )
+                        },
+                        onCheckboxChange = { setIndex, checked ->
+                            viewModel.editor.updateSet(setGroupIndex, setIndex, complete = checked)
+                        }
+                    )
+                }
+
+                item {
+                    Spacer(Modifier.height(16.dp))
+                    OutlinedButton(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(horizontal = 8.dp)
+                            .height(120.dp),
+                        onClick = {
+                            scope.launch {
+                                sheetState.show()
+                            }
+                        }
                     ) {
-                        Icon(Icons.Default.Done, null)
-                        Spacer(Modifier.width(8.dp))
-                        Text("Finish Workout")
+                        Icon(Icons.Default.Add, null)
+                        Spacer(Modifier.width(12.dp))
+                        Text("Add Exercise")
+                    }
+                }
+
+                item {
+                    Spacer(Modifier.height(16.dp))
+                    Row(
+                        Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp),
+                        horizontalArrangement = Arrangement.Center,
+                    ) {
+                        TextButton(
+                            modifier = Modifier.weight(1f),
+                            onClick = { showCancelWorkoutDialog = true },
+                        ) {
+                            Icon(Icons.Default.Delete, null)
+                            Spacer(Modifier.width(8.dp))
+                            Text("Delete Workout")
+                        }
+                        Spacer(Modifier.width(16.dp))
+                        Button(
+                            modifier = Modifier.weight(1f),
+                            onClick = { showFinishWorkoutDialog = true }
+                        ) {
+                            Icon(Icons.Default.Done, null)
+                            Spacer(Modifier.width(8.dp))
+                            Text("Finish Workout")
+                        }
                     }
                 }
             }
