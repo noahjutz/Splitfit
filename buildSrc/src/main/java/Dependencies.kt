995226cc0d75a51c0b900f965756997fd2ce0edd
Noah Jutz <noahjutz@tutanota.de>
2021-06-29 05:57:26 +0200

    Upgrade Koin to v3.1.1

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -88,7 +88,7 @@ object Libs {
     }
 
     object Koin {
-        private const val version = "3.1.0"
+        private const val version = "3.1.1"
         const val android = "io.insert-koin:koin-android:$version"
         const val compose = "io.insert-koin:koin-androidx-compose:$version"
         const val test = "io.insert-koin:koin-test:$version"
