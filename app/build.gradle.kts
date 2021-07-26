9e5d470e5819c63254cbd1c0c4b5e7708b533b37
Noah Jutz <noahjutz@tutanota.de>
2021-07-26 11:04:58 +0200

    Update version to v0.1.0-alpha21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.noahjutz.splitfit"
         minSdk = App.minSdk
         targetSdk = App.targetSdk
-        versionCode = 31
-        versionName = "0.1.0-alpha20"
+        versionCode = 32
+        versionName = "0.1.0-alpha21"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
