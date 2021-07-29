b5119189e9f2b2c76ffcd6198235a232273211d1
Noah Jutz <noahjutz@tutanota.de>
2021-07-29 17:49:45 +0200

    Update WorkoutViewer layout

--- app/src/main/java/com/noahjutz/splitfit/ui/workout/viewer/WorkoutViewer.kt
+++ app/src/main/java/com/noahjutz/splitfit/ui/workout/viewer/WorkoutViewer.kt
@@ -1,20 +1,18 @@
 package com.noahjutz.splitfit.ui.workout.viewer
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
+import androidx.compose.material.MaterialTheme.typography
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.noahjutz.splitfit.data.domain.Workout
 import com.noahjutz.splitfit.data.domain.duration
@@ -31,40 +29,43 @@ fun WorkoutViewer(
     viewModel: WorkoutViewerViewModel = getViewModel { parametersOf(workoutId) },
     popBackStack: () -> Unit,
 ) {
-    val workout by viewModel.workout.collectAsState()
-    if (workout != null) WorkoutViewer(popBackStack, workout!!)
-}
-
-@Composable
-fun WorkoutViewer(
-    popBackStack: () -> Unit,
-    workout: Workout
-) {
     Scaffold(
         topBar = {
             TopBar(
-                title = workout.name,
+                title = "View Workout",
                 navigationIcon = {
                     IconButton(onClick = popBackStack) { Icon(Icons.Default.ArrowBack, null) }
                 }
             )
         }
     ) {
-        LazyColumn {
-            item {
-                Text(
-                    text = workout.duration.iso8601(),
-                    Modifier
-                        .fillMaxWidth()
-                        .padding(16.dp),
-                    textAlign = TextAlign.Center
-                )
-                Divider()
-            }
+        val workout by viewModel.workout.collectAsState()
+        if (workout != null) WorkoutViewerContent(workout!!)
+    }
+}
 
-            items(workout.setGroups) {
-                Text(it.sets.toString()) // TODO use SetGroupCards
-            }
+@Composable
+fun WorkoutViewerContent(workout: Workout) {
+    LazyColumn {
+        item {
+            Spacer(Modifier.height(16.dp))
+            Text(
+                text = workout.name,
+                modifier = Modifier.padding(horizontal = 16.dp),
+                style = typography.h4,
+            )
+            Text(
+                text = workout.duration.iso8601(),
+                modifier = Modifier.padding(horizontal = 16.dp),
+            )
+            Spacer(Modifier.height(16.dp))
+            Divider()
+            Spacer(Modifier.height(16.dp))
+        }
+
+        items(workout.setGroups) {
+            Text(it.sets.toString()) // TODO use SetGroupCards
+            Spacer(Modifier.height(12.dp))
         }
     }
 }
